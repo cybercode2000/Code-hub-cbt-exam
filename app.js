762d0c05b21a9832ca4e6bad4e2bf47fbabe4590
Cybercode <damicodd123@gmail.com>
2019-08-08 14:32:55 -0400

    uploading files

--- app.js
+++ app.js
@@ -1,45 +1,79 @@
-let username = document.querySelector('#matric');
-let password = document.querySelector('#password');
-let btn = document.querySelector('#login');
-let link = document.querySelector('#link');
-let err = document.querySelector('#error');
-function login(){
-    let matricnuumber = ['17/7cs/081', '17/7cs/082', '17/7cs/083', '17/7cs/084'];
-    let pass = ['damilare', 'koko', 'code', 'cybercode'];
-    if(username.value === matricnuumber[0] && password.value === pass[0] || username.value === matricnuumber[1] && password.value === pass[1] || username.value === matricnuumber[2] && password.value === pass[2] || username.value === matricnuumber[3] && password.value === pass[3] || username.value === matricnuumber[4] && password.value === pass[4])
-        link.href="instructionpage.html";
+document.querySelector('#logout').addEventListener('click', function(){
+    if(confirm('are you sure you want to log out') == true)
+    logoutlink.href = 'index.html';
     else
-    {
-        error.style.display = 'block';
-        username.style.border = '1px solid Red';
-        password.style.border = '1px solid Red';
-    }
-}
-
-link.addEventListener('click', login);
-
+    logoutlink.href = '#';
+});
+let opt1 = document.querySelector('.a');
+let opt2 = document.querySelector('.b');
+let opt3 = document.querySelector('.c');
+let question1 = document.querySelector('#introp');
+let option1 = document.querySelector('#option1');
+let option2 = document.querySelector('#option2');
+let option3 = document.querySelector('#option3');
+let damicode = Math.floor(Math.random() * (10-5)+5);
 let courses = [
-        ['what is your name',
+        ['what is the full meaning of \'HTML\' ',
         'rasheed', 'damicode', 'damilare'
         ],
-        ['my name is damicode', 'rasheed', 'damicode', 'damilare'],
-        ['what is your name', 'rasheed', 'damicode', 'damilare'],
-        ['my name is damicode', 'rasheed', 'damicode', 'damilare'],
-        ['what is your name', 'rasheed', 'damicode', 'damilare'],
-        ['my name is damicode', 'rasheed', 'damicode', 'damilare'],
-        ['what is your name', 'rasheed', 'damicode', 'damilare'],
-        ['my name is damicode', 'rasheed', 'damicode', 'damilare'],
-        ['what is your name', 'rasheed', 'damicode', 'damilare'],
-        ['my name is damicode', 'rasheed', 'damicode', 'damilare']
+        ['What is the Full meaning of \"HTML\" ', 'Hypertext Markup Language', 'Hyberbola toy language', 'Hyper Markup Language', 'C'],
+        ['What is the full meaning of \"Css\" ', 'Cholera Style Sheet', 'Cascading Style Sheet', 'Kolawole Style Sheet', 'B'],
+        ['What is the full meaning of \"JS\" ', 'Javascript', 'Jango', 'Jolayemi Aremu', 'A'],
+        ['What is the name of your School ', 'KWASU', 'Cancel', 'Shit Hole', 'A'],
+        ['What is the full meaning of \"PHP\" ', 'Personal Home page', 'Hypertext Preprocessor', 'Pause Hole People', 'B'],
+        ['What is the full meaning of \"KWASU\" ', 'Kwara State University', 'Kolawole State University', 'Khadija State University', 'A'],
+        ['What is the Name of Kwasu Vice  Chancellor ', 'Abdul-Rahman Nallah', 'Abdul-Rasheed Na-Allah', 'Abolade kunle', 'B'],
+        ['What is the Name of my  best Lecturer ', 'Dr. Mrs Babatunde', 'Mrs. Shakirat', 'Dr. Mrs Ajao', 'A'],
+        ['What is the name of the Guy that build Me ', 'Damilare', 'waliyillahi', 'fawaz', 'A']
     ];
-    let damicode = Math.floor(Math.random() * (10-5)+5);
-    let rasheed = courses[damicode][0];
-    let option1 = courses[damicode][2];
-    let option2 = courses[damicode][1];
-    let option3 = courses[damicode][3];
 
-console.log(rasheed);
-console.log(option1);
-console.log(option2);
-console.log(option3);
+    let answer = courses[damicode][4];
+    let question = courses[damicode][0]
+    question1.textContent = question;
+    option1.textContent = courses[damicode][1];
+    option2.textContent = courses[damicode][2];
+    option3.textContent = courses[damicode][3];
+    let score = 0;
+    
+    let pagenumber = document.querySelector('#wole');
+let koko = document.querySelector('#btn2');
+koko.addEventListener('click',caculate);
+
+
+function caculate(){
+result.textContent = opt1.checked == true && answer == 'A' ? score +=1 : opt2.checked === true && answer == 'B' ? score +=1 : opt3.checked === true && answer == 'C' ? score +=1 : score +=0;
+}
+
+
+
+
+// if(pagenumber.textContent == '1'){
+    //     console.log(opt1.checked == true && answer == 'A' ? score +=1 : opt2.checked === true && answer == 'B' ? score +=1 : opt3.checked === true && answer == 'C' ? score +=1 : score +=0);
+    //  }
+    //  else if(pagenumber.textContent == '2'){
+    //     console.log(opt1.checked == true && answer == 'A' ? score +=1 : opt2.checked === true && answer == 'B' ? score +=1 : opt3.checked === true && answer == 'C' ? score +=1 : score +=0);    }
+    //  else if(pagenumber.textContent == '3'){
+    //     console.log(opt1.checked == true && answer == 'A' ? score +=1 : opt2.checked === true && answer == 'B' ? score +=1 : opt3.checked === true && answer == 'C' ? score +=1 : score +=0);     }
+    //  else if(pagenumber.textContent == '4'){
+    //     console.log(opt1.checked == true && answer == 'A' ? score +=1 : opt2.checked === true && answer == 'B' ? score +=1 : opt3.checked === true && answer == 'C' ? score +=1 : score +=0);
+    //  }
+    //  else if(pagenumber.textContent == '5'){
+    //     console.log(opt1.checked == true && answer == 'A' ? score +=1 : opt2.checked === true && answer == 'B' ? score +=1 : opt3.checked === true && answer == 'C' ? score +=1 : score +=0);     
+    //  }
+    //  else 
+    //  console.log('not exist');
+// if(pagenumber.textContent == '1'){
+//     result1();
+// }
+// else if(pagenumber.textContent == '2'){
+
+//     result1();
+// }
+// else if(pagenumber.textContent == '3'){
+//     result1();
+// }
+// else {
+//     console.log('me');
+// }
+
 
